57f3a83d1f41f1fb73e05e58115dcac51da4cd11
Brenda Joshua <brendajosh.bj@gmail.com>
2019-09-30 16:14:15 +0300

    deleted constructor and state from TodoForm.js

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,39 +1,20 @@
 import React from 'react';
 
 
-class TodoForm extends React.Component{
-    constructor(){
-        super()
-        this.state = {
-            task: ''
-        }
-    }
-
-    handleChanges = event => {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-    };
-
-    submitTask = event => {
-        event.preventDefault();
-        this.props.addTask(this.state.task);
-    };
-
-    render(){
-        return(
-            <form onSubmit={this.submitTask}>
-                <input
-                type="text"
-                value={this.task}
-                name="task"
-                onChange={this.handleChanges}
-                placeholder="Add a new Todo task here!"/>
-                <button>Add Todo</button>
-                <button onClick={this.props.clearCompleted}>Clear</button>
-            </form>
-        )
-    }
-}
-
-export default TodoForm;
+const TodoForm = props => {
+    return (
+      <form>
+        <input
+          onChange={props.handleTodoChange}
+          type="text"
+          name="todo"
+          value={props.value}
+          placeholder="add task"
+        />
+        <button onClick={props.handleAddTodo}>Add Todo</button>
+        <button onClick={props.handleClearTodos}>Clear Completed</button>
+      </form>
+    );
+  };
+  
+  export default TodoForm;
